0c82ecf3da289dc1288fd06f5921c778a84ae846
rahman_abdul <arsain88.P>
2021-02-03 17:22:51 +0500

    initial git

--- lib/UI/flights_screen.dart
+++ lib/UI/flights_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
 
 class FlightsScreen extends StatefulWidget {
   @override
@@ -18,7 +17,23 @@ class _FlightsScreenState extends State<FlightsScreen> {
               initialCameraPosition: CameraPosition(
             target: LatLng(24.8607, 67.0011),
             zoom: 15,
-          ))
+          )),
+          Positioned(
+            bottom: 10,
+            left: 10,
+            right: 10,
+            child: SizedBox(
+              height: 60.0,
+              width: 60.0,
+              child: Container(
+                  child: ListView.builder(
+                      itemCount: 1,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Card();
+                      })),
+            ),
+          ),
         ],
       ),
     );
