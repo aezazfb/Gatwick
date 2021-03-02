ae8acada9ef3d3adc5e5873b3517449fa8a4d40d
rahman_abdul <arsain88.P>
2021-03-02 17:08:47 +0500

    initial git

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -46,6 +46,12 @@
 @import share;
 #endif
 
+#if __has_include(<shared_preferences/FLTSharedPreferencesPlugin.h>)
+#import <shared_preferences/FLTSharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
@@ -56,6 +62,7 @@
   [FLTGoogleMapsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleMapsPlugin"]];
   [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
   [FLTSharePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharePlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
 }
 
 @end
--- lib/requests/map_screen/detailsRequest.dart
+++ lib/requests/map_screen/detailsRequest.dart
@@ -1,12 +1,12 @@
-import 'package:http/http.dart' as http;
-
 import 'package:basic_utils/basic_utils.dart';
+import 'package:http/http.dart' as http;
+import 'package:requests/requests.dart';
+
 import 'dart:convert';
 import 'package:zippy_rider/GooglePlaces.dart';
 
 class LocationDetails {
-  GooglePlaces _origin;
-  GooglePlaces _destination;
+  GooglePlaces googlePlaces = new GooglePlaces();
 
   Future<Map<String, dynamic>> getLocationDetails(String value) async {
     Map mapResponse;
@@ -15,8 +15,33 @@ class LocationDetails {
     response = await http.get(url);
     mapResponse = json.decode(response.body);
     Map<String, dynamic> map = mapResponse;
+
+    var pickUp = {
+      "",
+      "ChIJ7ZFe_0gCdkgRRcALA0ZN5EE",
+      "Brockley Road, London SE4 2BY, UK",
+      " SE4 2BY",
+      "SE4",
+      51.454334,
+      "United Kingdom",
+      "Greater London",
+      -0.0379635
+    };
+
+    var dropOff = {
+      "",
+      "ChIJt3-FKDsKdkgRrsljcgmJThU",
+      "Surbiton KT6 7QD, UK",
+      "KT6 7QD",
+      "KT6",
+      51.3751638,
+      "United Kingdom",
+      "",
+      -0.293779
+    };
+
     Map<String, dynamic> pickup = {
-      "_id": 'null',
+      "_id": "",
       "placeid": "ChIJ7ZFe_0gCdkgRRcALA0ZN5EE",
       "address": "Brockley Road, London SE4 2BY, UK",
       "postcode": "SE4 2BY",
@@ -28,50 +53,63 @@ class LocationDetails {
     };
 
     Map<String, dynamic> dropoff = {
-      '_id': 'null',
-      'placeid':
-          'EiBMZXdpc2hhbSBXYXksIExvbmRvbiBTRTQgMVVZLCBVSyIuKiwKFAoSCXk1YsdYAnZIERCJXqRiE8WPEhQKEgnrjwApXwJ2SBH76fXJO6C02w',
-      'address': 'Lewisham Way, London SE4 1UY, UK',
-      'postcode': 'SE4 1UY',
-      'outcode': 'SE4 1UY',
-      'lattitude': 51.4702816,
-      'country': 'United Kingdom',
-      'city': 'Greater London',
-      'longitude': -0.029187800000045172,
+      "_id": "",
+      "placeid": "",
+      "address": "Kenley Road, London SW19 3DW, UK",
+      "postcode": "SE23 3RF",
+      "outcode": "SE23",
+      "lattitude": 51.404556,
+      "country": "",
+      "city": "",
+      "longitude": -0.194486
     };
-    var a = jsonEncode(pickup);
-    var b = jsonEncode(dropoff);
-    List<dynamic> list = [a, b];
 
-/*
-    var url4 = 'http://testing.thedivor.com/api/API/GetDistance?${a},${b}';
-    final response1 = await http.get(url4);
-    print(url4.toString());
-    print("__________________________________${response1.body} ");
-*/
+    // var url1 = 'http://testing.thedivor.com/api/API/GetDistance?${list[0]},${list[1]}';
+    // print(url1);
+    // final response1 = await http.post(url1,null,list,Json);
+    // print("__________________________________${response1.body}");
 
-    Map<String, String> queryParameters = {
-      'pickup': pickup.toString(),
-      'dropoff': dropoff.toString(),
-    };
-    var url3 =
-        "http://testing.thedivor.com/api/API/GetDistance?$queryParameters";
-    String qureyString = Uri(queryParameters: queryParameters).query;
-    var endpointUrl = 'http://testing.thedivor.com/api/API/GetDistance?';
+    // var urlpp = 'http://testing.thedivor.com/api/API/GetDistance';
+    // var responsepp = await http.post(urlpp,body: list);
+    // print('Response status: ${responsepp.statusCode}');
+    // print('Response body: ${responsepp.body}');
 
-    var requestUrl = endpointUrl + '?' + qureyString;
-    var repu = await http.get(requestUrl);
-    print('ittttsss_______________${repu.body}');
-    var uri = Uri.https(
-        'testing.thedivor.com', '/api/API/GetDistance?', queryParameters);
+    // print(await http.read('https://example.com/foobar.txt'));
 
-    Map<String, String> headers = {"Accept": "application/json"};
+    var req = await Requests.post(
+        'http://testing.thedivor.com/api/API/GetDistance',
+        headers: {'Content-Type': 'application/json'},
+        body: {pickup, dropoff});
+    print(
+        'response_____________O______________)___________________0___________O_________\n');
+    print(req.json());
+    print(req.content());
+    if (req.statusCode == 200) {
+      print(
+          'response_____________O______________)___________________0___________O_________\n');
+    }
 
-// Body
-    String body = "[{'pickup':$pickup},{'dropoff': $dropoff}]";
-// Send request
-    var responseData = await HttpUtils.postForJson(
-        "http://testing.thedivor.com/api/API/GetDistance?", body);
-    print(responseData);
+//
+//     var url3 =
+//         "http://testing.thedivor.com/api/API/GetDistance?$queryParameters";
+//     String qureyString = Uri(queryParameters: queryParameters).query;
+//     var endpointUrl = 'http://testing.thedivor.com/api/API/GetDistance?';
+//
+//     var requestUrl = endpointUrl + '?' + qureyString;
+//     var repu = await http.get(requestUrl);
+//     print('ittttsss_______________${repu.body}');
+//     var uri = Uri.https(
+//         'testing.thedivor.com', '/api/API/GetDistance?', queryParameters);
+//
+//     Map<String, String> headers = {"Accept": "application/json"};
+//
+//Body
+//     String body = "[{$pickUp},{$dropOff}]";
+// // Send request
+//     var responseData = await HttpUtils.postForJson(
+//         "http://testing.thedivor.com", body);
+//     print(responseData.values);
+
+    return map;
   }
 }
